cfe3ee463ae87b334e8f0554c32b8968d4c46df5
h0tcat <h0tnori@vivaldi.net>
2021-04-09 19:43:18 +0900

    いろいろ修正

--- nisin.cpp
+++ nisin.cpp
@@ -8,33 +8,46 @@
 
 int main() {
 
-	std::vector<bool> nisin,withHugouNisin;
-
-	nisin.resize(8);
+	
+	std::vector<bool> nisin(8);
 
 	std::fill(nisin.begin(),nisin.end(),false);
-
 	int zyusin;
 
 	std::cout << "数を入力してください : ";
 	std::cin >> zyusin;
 
-	if (zyusin < -255 || zyusin > 255)
-		std::cout << "-256より大きく256未満で入力してください" << std::endl;
+	if (zyusin > 255)
+		std::cout << "256未満で入力してください" << std::endl;
 	else {
-		if(zyusin<0){
-			//もし負の値だったら、別対応する。
-			std::stringstream strStream;
-			strStream << std::bitset<8>(zyusin);
-			std::cout << strStream.str() << std::endl;
-		}else{
+		if(zyusin>=0){
+
 			for (int i = 0; zyusin != 0; i++) {
 				nisin[i] = zyusin % 2;
 				zyusin /= 2;
 			}
-			for (int i = (sizeof(nisin) / sizeof(nisin[0]) - 1); i >= 0; i--) {
-				std::cout << nisin[i];
-			}
+			
+			std::reverse(nisin.begin(),nisin.end());
+			for(auto binary : nisin)
+				std::cout << binary;
+
+			/*
+			 * おまけのコード
+			 *
+			 *
+			 * std::stringstream strStream;
+			 * strStream << std::bitset<8>(zyusin);
+			 * std::cout << strStream.str() << std::endl;
+			 *
+			 *
+			*/
+		}else{
+
+			//もし負の値だったら、別対応する。
+			std::stringstream strStream;
+			strStream << std::bitset<8>(zyusin);
+			std::cout << strStream.str() << std::endl;
 		}
+		std::cout  << std::endl;
 	}
 }
